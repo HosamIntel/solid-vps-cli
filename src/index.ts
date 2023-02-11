40f8681b3e727a2a5eec55491d3f85125ef8b07b
Hosam <hosamintel@gmail.com>
2023-02-11 17:19:28 +0330

    feat: creating the project

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import minimist from 'minimist';
 import { blue, red, reset, yellow } from 'kolorist';
 import path from 'node:path';
 import prompts from 'prompts';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
 
 const cwd = process.cwd();
 
@@ -40,7 +42,7 @@ const templates = options.map((option: Option) => {
 const defaultProjectDir = 'solid-js-app';
 
 async function run() {
-  const argProjectDir = argv._[0];
+  const argProjectDir = formatTargetDir(argv._[0]);
   const argTemplate = argv.template;
 
   let projectDir = argProjectDir || defaultProjectDir;
@@ -58,7 +60,7 @@ async function run() {
           message: reset('Project name:'),
           initial: defaultProjectDir,
           onState: (state) => {
-            projectDir = state.value || defaultProjectDir;
+            projectDir = formatTargetDir(state.value) || defaultProjectDir;
           },
         },
         {
@@ -95,12 +97,95 @@ async function run() {
   const { option } = result;
 
   const root = path.join(cwd, projectDir);
-  console.log(root, option?.name || argTemplate);
-  
+  const template = option ? option.name : argTemplate;
+  const pkgInfo = pkgFromUserAgent(process.env.npm_config_user_agent);
+  const pkgManager = pkgInfo ? pkgInfo.name : 'npm';
+
+  fs.mkdirSync(root, { recursive: true });
+
+  console.log(`\nScaffolding project in ${root}`);
+
+  const localTemplateDir = path.resolve(
+    fileURLToPath(import.meta.url),
+    '../..',
+    `${template}-template`
+  );
+
+  const write = (file: string, content?: string) => {
+    const targetPath = path.join(root, file);
+    if (content) {
+      fs.writeFileSync(targetPath, content);
+    } else {
+      copy(path.join(localTemplateDir, file), targetPath);
+    }
+  };
+
+  const pkg = JSON.parse(
+    fs.readFileSync(path.join(localTemplateDir, `package.json`), 'utf-8')
+  );
+
+  pkg.name == getProjectName();
+
+  write('package.json', JSON.stringify(pkg, null, 2));
+
+  const files = fs.readdirSync(localTemplateDir);
+  files
+    .filter((f) => f !== 'package.json')
+    .forEach((file) => {
+      write(file);
+    });
+
+  console.log(`\nDone. Now run:\n`);
+  if (root !== cwd) {
+    console.log(`  cd ${path.relative(cwd, root)}`);
+  }
+  switch (pkgManager) {
+    case 'yarn':
+      console.log('  yarn');
+      console.log('  yarn dev');
+      break;
+    default:
+      console.log(`  ${pkgManager} install`);
+      console.log(`  ${pkgManager} run dev`);
+      break;
+  }
+  console.log();
 }
 
 try {
   run();
 } catch (error) {
   console.log('error Occurred');
+}
+
+function formatTargetDir(targetDir: string | undefined) {
+  return targetDir?.trim().replace(/\/+$/g, '');
+}
+
+function copyDir(srcDir: string, destDir: string) {
+  fs.mkdirSync(destDir, { recursive: true });
+  for (const file of fs.readdirSync(srcDir)) {
+    const srcFile = path.resolve(srcDir, file);
+    const destFile = path.resolve(destDir, file);
+    copy(srcFile, destFile);
+  }
+}
+
+function copy(src: string, dest: string) {
+  const stat = fs.statSync(src);
+  if (stat.isDirectory()) {
+    copyDir(src, dest);
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+}
+
+function pkgFromUserAgent(userAgent: string | undefined) {
+  if (!userAgent) return undefined;
+  const pkgSpec = userAgent.split(' ')[0];
+  const pkgSpecArr = pkgSpec.split('/');
+  return {
+    name: pkgSpecArr[0],
+    version: pkgSpecArr[1],
+  };
 }
