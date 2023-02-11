8e84b2ebb42eaa147b0f661b17a7828ba56fd043
Hosam <hosamintel@gmail.com>
2023-02-11 04:34:18 +0330

    feat: data gathering

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,26 @@
 import minimist from 'minimist';
-import {
-  blue,
-  cyan,
-  yellow,
-  bgYellow
-} from 'kolorist';
+import { blue, red, reset, yellow } from 'kolorist';
+import path from 'node:path';
+import prompts from 'prompts';
 
 const cwd = process.cwd();
 
-const argv = minimist<{
-  typescript?: string;
-  telefunc?: string;
-  tailwind?: string;
-}>(process.argv.slice(2), { string: ['_'] });
+const argv = minimist<{ template?: string; t?: string }>(
+  process.argv.slice(2),
+  {
+    string: ['_'],
+  }
+);
 
 type ColorFunc = (str: string | number) => string;
-type Template = {
+
+type Option = {
   name: string;
   display: string;
   color: ColorFunc;
 };
 
-type Options = {
-  name: string;
-  display: string;
-  color: ColorFunc;
-};
-
-const templates: Template[] = [
+const options: Option[] = [
   {
     name: 'solid',
     display: 'Javascript',
@@ -40,21 +33,74 @@ const templates: Template[] = [
   },
 ];
 
-const options: Options[] = [
-  {
-    name: 'telefunc',
-    display: 'Telefunc',
-    color: bgYellow
-  },
-  {
-    name: 'tailwind',
-    display: 'Tailwind.Css',
-    color: cyan
-  },
-] 
+const templates = options.map((option: Option) => {
+  return option.name;
+});
 
-const renameFiles: Record<string, string | undefined> = {
-  _gitignore: '.gitignore',
-};
+const defaultProjectDir = 'solid-js-app';
 
-const defaultTargetDir = 'solid-js-app';
+async function run() {
+  const argProjectDir = argv._[0];
+  const argTemplate = argv.template;
+
+  let projectDir = argProjectDir || defaultProjectDir;
+  const getProjectName = () =>
+    projectDir === '.' ? path.basename(path.resolve()) : projectDir;
+
+  let result: prompts.Answers<'projectName' | 'option'>;
+
+  try {
+    result = await prompts(
+      [
+        {
+          type: argProjectDir ? null : 'text',
+          name: 'projectName',
+          message: reset('Project name:'),
+          initial: defaultProjectDir,
+          onState: (state) => {
+            projectDir = state.value || defaultProjectDir;
+          },
+        },
+        {
+          type:
+            argTemplate && templates.includes(argTemplate) ? null : 'select',
+          name: 'option',
+          message:
+            typeof argTemplate === 'string' && !templates.includes(argTemplate)
+              ? reset(
+                  `"${argTemplate}" isn't a valid template. Please choose from below: `
+                )
+              : reset('Select an Option'),
+          initial: 0,
+          choices: options.map((option) => {
+            const frameworkColor = option.color;
+            return {
+              title: frameworkColor(option.display || option.name),
+              value: option,
+            };
+          }),
+        },
+      ],
+      {
+        onCancel: () => {
+          throw new Error(red('✖') + ' Operation cancelled');
+        },
+      }
+    );
+  } catch (cancelled: any) {
+    console.log(cancelled.message);
+    return;
+  }
+
+  const { option } = result;
+
+  const root = path.join(cwd, projectDir);
+  console.log(root, option?.name || argTemplate);
+  
+}
+
+try {
+  run();
+} catch (error) {
+  console.log('error Occurred');
+}
